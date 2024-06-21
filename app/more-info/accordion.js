33e8e083209b2b71daf5092907cd9851f57f55cf
cx18121 <charlie.l.xue@gmail.com>
2024-06-21 16:52:29 -0400

    commit

--- app/more-info/accordion.js
+++ app/more-info/accordion.js
@@ -11,7 +11,7 @@ const Accordion = ({ question, answer }) => {
         className="w-full py-4 text-left focus:outline-none"
         onClick={() => setIsOpen(!isOpen)}
       >
-        <h3 className="text-lg font-medium ml-2 hover:font-bold">{question}</h3>
+        <h3 className="text-lg font-medium ml-2 hover:underline">{question}</h3>
       </button>
       {isOpen && <p className="px-4 pb-4">{answer}</p>}
     </div>
