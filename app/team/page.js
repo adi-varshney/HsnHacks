75ae3cfabb8b2ce4088be0239fd31d2276502918
PrathyushY <prathyushyeturi@gmail.com>
2024-10-01 22:50:01 -0400

    Updated the meet the team page to include all of the members and what group they are in.

--- app/team/page.js
+++ app/team/page.js
@@ -17,12 +17,9 @@ export default function Team() {
     );
   };
 
-  const Card = ({ name, imageURL, about }) => {
+  const Card = ({ name, about }) => {
     return (
       <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 m-4">
-        <a href="#">
-          <img className="rounded-t-lg w-full h-48 object-cover" src={imageURL} alt={name} />
-        </a>
         <div className="p-5">
           <a href="#">
             <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{name}</h5>
@@ -36,53 +33,117 @@ export default function Team() {
   return (
     <div>
       <NavBar />
-      <div className="p-10">
-        <div className="text-center mb-12 flex justify-center items-center">
+      <div className="text-center">
+        <div className="text-center mb-12 flex justify-center items-center py-5">
           <img src="/girlsWhoCode.png" alt="Girls Who Code" className="h-16 w-auto mr-4" />
           <h1 className="text-6xl">Meet the Team</h1>
           <img src="/CsClub.png" alt="CS Club" className="h-16 w-auto ml-4" />
         </div>
+
+        <div className="text-center mb-8">
+          <h2 className="text-4xl">Logistics</h2>
+        </div>
         <div className="flex flex-wrap justify-center">
           <Card
-            name="Name 1"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
+            name="Akshat Mehta"
+            about="Department Leader"
           />
           <Card
-            name="Name 2"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
+            name="Alex Wang"
+            about="Department Leader"
+          />
+          <div className="w-full"></div>
+          <Card
+            name="Prathyush Yeturi"
+            about="Member"
           />
           <Card
-            name="Name 3"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
+            name="Charlie Xue"
+            about="Member"
           />
           <Card
-            name="Name 4"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
+            name="Kashish Arora"
+            about="Member"
           />
           <Card
-            name="Name 5"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
+            name="Rajveer Dharkar"
+            about="Member"
           />
           <Card
-            name="Name 6"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
+            name="Arjun Mehan"
+            about="Member"
           />
-          <Card
-            name="Name 7"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
-          />
-          <Card
-            name="Name 8"
-            imageURL="https://via.placeholder.com/400x400"
-            about="A small paragraph about the person"
-          />
+        </div>
+
+        <div className="py-5">
+          <div className="text-center mb-8">
+            <h2 className="text-4xl">Sponsorship</h2>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            <Card
+              name="Audrey Chen"
+              about="Department Director"
+            />
+            <div className="w-full"></div>
+            <Card
+              name="Prathyush Yeturi"
+              about="Member"
+            />
+            <Card
+              name="Claire Cheng"
+              about="Member"
+            />
+            <Card
+              name="Kashish Arora"
+              about="Member"
+            />
+            <Card
+              name="Rajveer Dharkar"
+              about="Member"
+            />
+          </div>
+        </div>
+
+        <div className="py-5">
+          <div className="text-center mb-8">
+            <h2 className="text-4xl">Outreach</h2>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            <Card
+              name="Aliza Borker"
+              about="Department Director"
+            />
+            <div className="w-full"></div>
+            <Card
+              name="Anvitha Komarraju"
+              about="Member"
+            />
+            <Card
+              name="Simrun Sabhlok"
+              about="Member"
+            />
+          </div>
+        </div>
+
+        <div className="py-5">
+          <div className="text-center mb-8">
+            <h2 className="text-4xl">Curriculum</h2>
+          </div>
+          <div className="flex flex-wrap justify-center">
+            <Card
+              name="Adikesh Nathan"
+              about="Department Director"
+            />
+            <div className="w-full"></div>
+            <Card
+              name="Simrun Sabhlok"
+              about="Member"
+            />
+            <Card
+              name="Pragnaya Goteti"
+              about="Member"
+            />
+          </div>
         </div>
       </div>
     </div>
